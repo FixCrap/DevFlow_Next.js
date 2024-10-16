82e96ba4fc4125c682d1a9ef96020fea4284a9b1
FixCrap <george_andriopoulos@hotmail.gr>
2024-10-16 16:34:56 +0300

    done a few shits...

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -3,13 +3,12 @@
 import React from "react";
 
 import { useTheme } from "@/context/ThemeProvider";
+
 import {
 	Menubar,
 	MenubarContent,
 	MenubarItem,
 	MenubarMenu,
-	MenubarSeparator,
-	MenubarShortcut,
 	MenubarTrigger,
 } from "@/components/ui/menubar";
 import Image from "next/image";
@@ -24,7 +23,7 @@ const Theme = () => {
 				<MenubarTrigger className='focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200'>
 					{mode === "light" ? (
 						<Image
-							src='./assets/icons/sun.svg'
+							src='/assets/icons/sun.svg'
 							alt='sun'
 							width={20}
 							height={20}
@@ -32,21 +31,22 @@ const Theme = () => {
 						/>
 					) : (
 						<Image
-							src='./assets/icons/moon.svg'
-							alt='sun'
+							src='/assets/icons/moon.svg'
+							alt='moon'
 							width={20}
 							height={20}
 							className='active-theme'
 						/>
 					)}
 				</MenubarTrigger>
-				<MenubarContent className='absolute right right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400  dark:bg-dark-300'>
+				<MenubarContent className='absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300'>
 					{themes.map((item) => (
 						<MenubarItem
 							key={item.value}
-							className='flex items-center gap-4 px-2.5 py-2 dark:focusbg-dark-400'
+							className='flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400'
 							onClick={() => {
 								setMode(item.value);
+
 								if (item.value !== "system") {
 									localStorage.theme = item.value;
 								} else {
@@ -55,7 +55,7 @@ const Theme = () => {
 							}}>
 							<Image
 								src={item.icon}
-								alt='item.value'
+								alt={item.value}
 								width={16}
 								height={16}
 								className={`${mode === item.value && "active-theme"}`}
@@ -64,7 +64,7 @@ const Theme = () => {
 								className={`body-semibold text-light-500 ${
 									mode === item.value
 										? "text-primary-500"
-										: " text-dark100_light900"
+										: "text-dark100_light900"
 								}`}>
 								{item.label}
 							</p>
